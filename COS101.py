7e67cd05e20bf2c4f123a0befa98ca3898194661
olaife230 <oladimejiifeoluwa230@gmail.com>
2024-11-17 21:36:56 +0100

    Add files via upload

---
+++ COS101.py
@@ -0,0 +1,49 @@
+print("hello,oladimeji ifeoluwa mary.lets solve some math")
+
+print('what is the formula for the area of a square ?')
+input("side1*side2")
+print("let side1 be = 20 now pick your side2 and solve ")
+side2 = input('enter your side2')
+solution = 20*int(side2)
+print(solution)
+
+print('what about some physics ?')
+input("lets go")
+print('what is the formula for current ?')
+print('current ?, i think its')
+input('voltage/resistance')
+print("let your resistance be 50")
+volatage= input('enter your voltage')
+solution = int(volatage)/50
+print(solution)
+
+print('wow you are good lets go for something more complex ?')
+input("sure !")
+print('what is the formula for potential energy ?')
+input('mass*gravity*height')
+input('mass = 4,gravity = 5')
+height = input('enter your height')
+solution = 4*5*int(height)
+print(solution)
+
+print('find out the formula for the circumference of a circle and volume of a cylinder')
+input("ok,done. the formulas are:")
+print('for the circumference of a circle, formula = 2*3.142*R')
+input("R being the radius")
+print('2*3.142 = 9.43')
+radius = input('enter your radius')
+solution = 9.43*int(radius)
+print(solution)
+
+print(' and for the volume of a cube ! the formula = nr2h')
+input('n is pi which is 3.142,take radius to be = 3')
+height = input('eneter your height')
+solution = 3.142*3*2*int(height)
+print(solution)
+
+print('oladimeji_ifeoluwa_mary_BHU/24/04/10/0043')
+
+
+
+
+
